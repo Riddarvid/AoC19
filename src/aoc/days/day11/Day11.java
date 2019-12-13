dc67fc7ed04eb95c4ff90a5b5491d48ca015a12a
Riddarvid <riddarvid@gmail.com>
2019-12-13 02:55:59 +0100

    Yeet

---
+++ src/aoc/days/day11/Day11.java
@@ -0,0 +1,20 @@
+package aoc.days.day11;
+
+import aoc.days.Day;
+
+public class Day11 extends Day {
+    @Override
+    protected void part1() {
+
+    }
+
+    @Override
+    protected void part2() {
+
+    }
+
+    @Override
+    protected void setup() {
+
+    }
+}
